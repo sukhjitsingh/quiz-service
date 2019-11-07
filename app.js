9af9016ca25704b8f4c1ea1cc3dd8d45785e29e0
sukhjitsingh <hms2012ca@gmail.com>
2019-11-07 10:07:34 -0500

    enabled cors

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
